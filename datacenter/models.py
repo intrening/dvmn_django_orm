c1abbb0add32505ed718cbe900c9c473f7d40d43
intrening <wol87@ya.ru>
2019-06-26 11:29:37 +0300

    init

---
+++ datacenter/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+import datetime
+
+class Passcard(models.Model):
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    passcode = models.CharField(max_length=200, unique=True)
+    owner_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        if self.is_active:
+            return self.owner_name
+        return f'{self.owner_name} (inactive)'
+
+
+class Visit(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    passcard = models.ForeignKey(Passcard)
+    entered_at = models.DateTimeField()
+    leaved_at = models.DateTimeField(null=True)
+
+    def duration (self):
+      if self.leaved_at:
+        return self.leaved_at - self.entered_at
+      else:
+        time_now = datetime.datetime.now().replace(microsecond=0)
+        return time_now - self.entered_at
+    
+    def is_visit_long(self, minutes=60):
+      duration_minutes = self.duration().total_seconds() / 60
+      return duration_minutes > minutes
+
+    def __str__(self):
+        return "{user} entered at {entered} {leaved}".format(
+            user=self.passcard.owner_name,
+            entered=self.entered_at,
+            leaved= "leaved at " + str(self.leaved_at) if self.leaved_at else "not leaved"
+        )
---
+++ datacenter/storage_information_view.py
@@ -0,0 +1,19 @@
+from datacenter.models import Passcard
+from datacenter.models import Visit
+from django.shortcuts import render
+
+def storage_information_view(request):
+    visits = Visit.objects.filter(leaved_at = None)
+    non_closed_visits = []
+    for visit in visits:
+      non_closed_visits.append({
+        "who_entered": visit.passcard.owner_name,
+        "entered_at": visit.entered_at,
+        "duration":  visit.duration(),
+        "is_strange": visit.is_visit_long()
+      })
+
+    context = {
+        "non_closed_visits": non_closed_visits,  # не закрытые посещения
+    }
+    return render(request, 'storage_information.html', context)
